126b44053afacd5494b8c62aaf55a4e07fafa225
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-05 20:24:42 +0900

    feat(Turn): turn 객체 구현

---
+++ src/main/kotlin/Turn.kt
@@ -0,0 +1,8 @@
+@JvmInline
+value class Turn(
+    private val value: Int,
+) {
+    init {
+        require(value > 0) { "턴은 양수만 가능합니다." }
+    }
+}
---
+++ src/test/kotlin/TurnTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class TurnTest {
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0])
+    fun `턴은 음수면 게임이 진행이 안된다`(value: Int) {
+        assertThrows<IllegalArgumentException> {
+            Turn(value)
+        }
+    }
+}
