9e22becfafd1798461bd9ad7e1b4c09824e86225
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-05 19:12:11 +0900

    feat(Car): 자동차 전진 기능 구현

--- src/main/kotlin/Position.kt
+++ src/main/kotlin/Position.kt
@@ -2,6 +2,8 @@
 value class Position(
     val value: Int,
 ) {
+    operator fun plus(i: Int): Position = Position(this.value + 1)
+
     init {
         require(value >= 0) { "이동 거리는 음수일 수 없습니다." }
     }
--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -16,6 +16,16 @@ class CarTest {
         val car = Car(carName, position)
         Assertions.assertThat(car.position).isEqualTo(position)
     }
+
+    @Test
+    fun `자동차는 전진한다`() {
+        val carName = CarName("모찌")
+        val position = Position(0)
+        val car = Car(carName, position)
+
+        val moveCar = car.move()
+        Assertions.assertThat(moveCar.position).isEqualTo(position + 1)
+    }
 }
 
 class Car(
@@ -23,4 +33,10 @@ class Car(
     val position: Position = Position(0),
 ) {
     constructor(carName: CarName) : this(carName, Position(0))
+
+    fun move(): Car = Car(this.name, this.position + 1)
 }
+
+// 나는 왜 불변객체로 만들고 싶은가
+// 불변 객체의 이점
+// class와 data class의 차이
