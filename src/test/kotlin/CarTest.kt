ca2b5d9f6955ebff0dee4f84db7b3798b5e990a3
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-05 18:28:41 +0900

    feat(Car): 자동차의 이동거리 생성

--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -8,8 +8,19 @@ class CarTest {
         val car = Car(carName)
         Assertions.assertThat(car.name).isEqualTo(carName)
     }
+
+    @Test
+    fun `자동차는 위치를 가진다`() {
+        val carName = CarName("모찌")
+        val position = Position(0)
+        val car = Car(carName, position)
+        Assertions.assertThat(car.position).isEqualTo(position)
+    }
 }
 
 class Car(
     val name: CarName,
-)
+    val position: Position = Position(0),
+) {
+    constructor(carName: CarName) : this(carName, Position(0))
+}
