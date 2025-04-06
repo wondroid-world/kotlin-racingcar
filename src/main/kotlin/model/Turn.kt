6de15263cf2746fd5b0bccf08cdaa13c895b8450
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-06 15:41:49 +0900

    feat: turn만큼 자동차 게임 경주 진행

--- src/main/kotlin/model/Turn.kt
+++ src/main/kotlin/model/Turn.kt
@@ -2,9 +2,17 @@ package model
 
 @JvmInline
 value class Turn(
-    private val value: Int,
+    val value: Int,
 ) {
     init {
         require(value > 0) { "턴은 양수만 가능합니다." }
+    }
+
+    fun playGame(cars: Cars): Cars {
+        var newCars: Cars = cars
+        repeat(value) {
+            newCars = cars.play()
+        }
+        return newCars
     }
 }
--- src/test/kotlin/model/TurnTest.kt
+++ src/test/kotlin/model/TurnTest.kt
@@ -1,5 +1,7 @@
 package model
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -11,5 +13,19 @@ class TurnTest {
         assertThrows<IllegalArgumentException> {
             Turn(value)
         }
+    }
+
+    @Test
+    fun `턴만큼 게임을 자동차 경주를 진행하면, 자동차의 상태가 바뀐다`() {
+        val car1 = Car.from("모찌")
+        val car2 = Car.from("치치")
+        val car3 = Car.from("모모")
+
+        val cars = Cars.from(listOf(car1, car2, car3))
+
+        val turn = Turn(5)
+        val playedCars = turn.playGame(cars)
+
+        Assertions.assertThat(cars != playedCars).isTrue()
     }
 }
