116700326841ef80462cbc50348dbdfb938bbe2a
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-04-06 15:43:55 +0900

    refactor(CarName): 프로퍼티명 변경(name -> value)

--- src/main/kotlin/model/CarName.kt
+++ src/main/kotlin/model/CarName.kt
@@ -2,13 +2,13 @@ package model
 
 @JvmInline
 value class CarName(
-    private val name: String,
+    val value: String,
 ) {
     constructor() : this("채원")
 
     init {
-        require(name.isNotEmpty()) { "자동차 이름은 비어있을 수 없습니다." }
-        require(name.length < 5) { "자동차 이름은 5글자 이내입니다." }
+        require(value.isNotEmpty()) { "자동차 이름은 비어있을 수 없습니다." }
+        require(value.length < 5) { "자동차 이름은 5글자 이내입니다." }
     }
 }
 
