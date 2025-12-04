82b8c655f30da7b59f95d923eb8e6e2cd3ee29c2
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-04 14:09:22 +0900

    test(Cars) : 공동 우승자 검증 로직 추가
    - 우승자는 한 명이상일 수 있다.
    - 최대 위치가 같다면 공동 우승자가 될 수 있다.

--- src/test/kotlin/CarsTest.kt
+++ src/test/kotlin/CarsTest.kt
@@ -13,10 +13,29 @@ class CarsTest {
         val winners: List<Car> = cars.winners()
 
         // then
-        val excepted: List<Car> = listOf(winnerCar)
+        val expected: List<Car> = listOf(winnerCar)
         val actual = winners
 
-        assertThat(actual).isEqualTo(excepted)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `최대 위치가 같다면 공동 우승자가 될 수 있다`() {
+        // given
+        val winnerCar1 = Car("모모", 1)
+        val winnerCar2 = Car("치치", 1)
+        val loserCar = Car("채채", 0)
+        val cars = Cars(listOf(winnerCar1, winnerCar2, loserCar))
+
+        // when
+        val winners: List<Car> = cars.winners()
+
+        // then
+        val expected: Int = listOf(winnerCar1, winnerCar2).size
+        val actual = winners.size
+
+        assertThat(actual).isEqualTo(expected)
+        assertThat(winners).contains(winnerCar1, winnerCar2)
     }
 }
 
