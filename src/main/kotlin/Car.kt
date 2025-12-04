bed09e38eb97196c65a79c5c8f30fb3649b76897
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-04 13:40:03 +0900

    feat(Car) : Car 생성 시, CarName을 사용하도록 추가

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,8 +1,17 @@
 data class Car(
+    val name: CarName,
     val position: Int = 0
 ) {
+
     fun move(canMove: Boolean): Car {
         val newPosition = if (canMove) position + 1 else position
-        return Car(newPosition)
+        return Car(name, newPosition)
+    }
+
+    companion object {
+        operator fun invoke(value: String, position: Int = 0): Car {
+            val carName = CarName(value)
+            return Car(carName, position)
+        }
     }
 }
--- src/test/kotlin/CarTest.kt
+++ src/test/kotlin/CarTest.kt
@@ -1,11 +1,40 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class CarTest {
     @Test
+    fun `자동차 생성할 때, 이름이 있어야한다`() {
+        // given
+        val carName = CarName("모찌")
+
+        // then
+        assertDoesNotThrow { Car(name = carName) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["모", "모모", "모치치", "모모치치", "모모와치치"])
+    fun `자동차 이름에 1~5자 사이의 String 타입 글자를 넣어도 자동차가 생성이 된다`(name: String) {
+        // then
+        assertDoesNotThrow { Car(value = name) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["모모치치채채"])
+    fun `자동차 이름에 6자 이상의 String 타입 글자를 넣으면 자동차가 생성되지 않는다`(name: String) {
+        // then
+        assertThrows<IllegalArgumentException> { Car(value = name) }
+    }
+
+    @Test
     fun `자동차 초기 위치는 0이다`() {
         // given, when
-        val car = Car()
+        val carName = CarName("모찌")
+        val car = Car(name = carName)
 
         // then
         val expected = 0
@@ -17,7 +46,8 @@ class CarTest {
     @Test
     fun `자동차는 전진 조건에 만족하면, 위치가 1 증가한다`() {
         // given
-        val car = Car()
+        val carName = CarName("모찌")
+        val car = Car(name = carName)
         val canMove = true
 
         // when
@@ -33,7 +63,8 @@ class CarTest {
     @Test
     fun `자동차는 전진 조건에 만족하지 않으면, 위치는 그대로이다`() {
         // given
-        val car = Car()
+        val carName = CarName("모찌")
+        val car = Car(name = carName)
         val canMove = false
 
         // when
