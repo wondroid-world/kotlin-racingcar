af65be6b474fe78620a8d77ddf7a0d6847836aab
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-12-09 16:39:41 +0900

    refactor(CarName): value의 가시성은 private -> public 변경
    - val이기 때문에 외부에서 변경 가능성이 없고 value 값을 읽을 수 있도록 변경

--- src/main/kotlin/domain/CarName.kt
+++ src/main/kotlin/domain/CarName.kt
@@ -2,7 +2,7 @@ package domain
 
 @JvmInline
 value class CarName(
-    private val value: String,
+    val value: String,
 ) {
     init {
         require(value.length in 1..5) { "자동차의 이름은 1자에서 5자 사이입니다." }
